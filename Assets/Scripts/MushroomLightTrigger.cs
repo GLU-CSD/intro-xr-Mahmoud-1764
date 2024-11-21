68f62674549113a2d0a27aeec6497820442a0fc7
Mahmoud-1764 <mahmoudchuaibsuliman@gmail.com>
2024-11-21 23:21:05 +0100

    idk

---
+++ Assets/Scripts/MushroomLightTrigger.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushroomLightTrigger : MonoBehaviour
+{
+    [SerializeField] private Light mushroomlight;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("player"))
+        {
+            mushroomlight.enabled = true;
+            Debug.Log("licht aan");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        mushroomlight.enabled = false;
+        Debug.Log("Light uit");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
